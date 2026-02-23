a48a7c5b9e54832c94bdb9e4d1062a198085ee8b
ismai <ismail.inbox@mail.ru>
2026-02-23 21:58:10 +0300

    Реализовал возможность добавления корректировок для  графика

---
+++ services/practitioner/src/CC.PractitionerService/Infrastructure/Persistence/Migrations/20260223163458_AddTablesAdjustments.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CC.PractitionerService.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTablesAdjustments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "adjustments",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    work_schedule_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type = table.Column<int>(type: "integer", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    period_from = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    period_to = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_adjustments", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "adjustments");
+        }
+    }
+}
