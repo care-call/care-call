b9277cdc8c16df89a3d1d1d4bcbd121b77b62f48
ferdavs <ferdavs@naver.com>
2026-04-04 01:52:05 +0900

    Меняем букву на заглавную

--- services/appointment/src/CC.AppointmentService/Domain/Feedbacks/ProfessionalismScore.cs
+++ services/appointment/src/CC.AppointmentService/Domain/Feedbacks/ProfessionalismScore.cs
@@ -6,6 +6,6 @@ namespace CC.AppointmentService.Domain.Feedbacks;
 public readonly partial struct ProfessionalismScore
 {
     private static Validation Validate(byte value) => 
-        value is < 1 or > 5 ? Validation.Invalid("оценка не может быть меньше 0 или же больше 5") 
+        value is < 1 or > 5 ? Validation.Invalid("Оценка не может быть меньше 0 или же больше 5") 
             : Validation.Ok;
 }
