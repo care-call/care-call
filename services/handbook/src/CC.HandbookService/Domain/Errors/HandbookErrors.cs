0254e6c870506add00bdb7a67d08b1d395fc790c
Azot <nublo237@yandex.ru>
2026-04-14 15:09:28 +0300

    CCL-121 Переносит ошибки связанные с парсингом в инфраструктурный слой

--- services/handbook/src/CC.HandbookService/Domain/Errors/HandbookErrors.cs
+++ services/handbook/src/CC.HandbookService/Domain/Errors/HandbookErrors.cs
@@ -4,15 +4,6 @@ namespace CC.HandbookService.Domain.Errors;
 
 public static class HandbookErrors
 {
-    public static Error NotExist(string handbookTitle) => 
-        new ($"Справочник «{handbookTitle}» не существует");
-
-    public static Error MissingField(string cellName)
-        => new ($"Пропущенно поле в ячейке {cellName}");
-
-    public static Error EmptyField(string cellName)
-        => new ($"Не заполнено обязательное поле в ячейке {cellName}");
-
-    public static Error Unexpected()
-        => new ("Непредвиденная ошибка");
+    public static Error NotExist(string handbookTitle) =>
+        new($"Справочник «{handbookTitle}» не существует");
 }
