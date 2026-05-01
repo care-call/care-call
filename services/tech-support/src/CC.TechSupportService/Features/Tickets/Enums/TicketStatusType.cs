2e8aef2d3c0fc427a5e57425019f2550be8b466a
DzangYang <ismail.inbox@mail.ru>
2026-05-01 20:09:51 +0300

    CCL-195 Добавлен енам типа статуса тикетов

---
+++ services/tech-support/src/CC.TechSupportService/Features/Tickets/Enums/TicketStatusType.cs
@@ -0,0 +1,13 @@
+﻿namespace CC.TechSupportService.Features.Tickets.Enums;
+
+public enum TicketStatusType
+{
+    New,
+    Opened,
+    InProgress,
+    WaitingForUser,
+    Escalated,
+    Resolved,
+    Reopened,
+    Closed
+}
