a573c5ff6941217582fded985a4ade85e0d4ab22
Azot <nublo237@yandex.ru>
2026-05-10 01:10:02 +0300

    CCl-214 Рефакторит IntervalSqlExecutor

--- services/tech-support/src/CC.TechSupportService/Infrastructure/BackgroundServices/SqlExecutor/IntervalSqlExecutor.cs
+++ services/tech-support/src/CC.TechSupportService/Infrastructure/BackgroundServices/SqlExecutor/IntervalSqlExecutor.cs
@@ -2,31 +2,14 @@ using Npgsql;
 
 namespace CC.TechSupportService.Infrastructure.BackgroundServices.SqlExecutor;
 
-public class IntervalSqlExecutor: BackgroundService
+public class IntervalSqlExecutor(
+    NpgsqlDataSource dataSource,
+    string sql,
+    NpgsqlParameter[] parameters,
+    TimeSpan interval,
+    ILogger logger) : BackgroundService
 {
-    private readonly NpgsqlDataSource _dataSource;
-    private readonly string _sql;
-    private readonly NpgsqlParameter[] _parameters;
-    private readonly ILogger _logger;
-    private readonly TimeSpan _interval;
-
-    private TimeSpan _currInterval;
     private readonly TimeSpan _retryInterval = TimeSpan.FromMinutes(1);
-    
-    public IntervalSqlExecutor(
-        NpgsqlDataSource dataSource,
-        string sql,
-        NpgsqlParameter[] parameters,
-        TimeSpan interval,
-        ILogger logger)
-    {
-        _dataSource = dataSource;
-        _sql = sql;
-        _parameters = parameters;
-        _interval = interval;
-        _currInterval = interval;
-        _logger = logger;
-    }
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -34,24 +17,26 @@ public class IntervalSqlExecutor: BackgroundService
         {
             try
             {
-                await using var conn = await _dataSource.OpenConnectionAsync(stoppingToken);
+                await using var conn = await dataSource.OpenConnectionAsync(stoppingToken);
                 await using var cmd = conn.CreateCommand();
                 
-                cmd.CommandText = _sql;
-                cmd.Parameters.AddRange(_parameters);
+                cmd.CommandText = sql;
+                cmd.Parameters.AddRange(parameters);
                 
                 await cmd.ExecuteNonQueryAsync(stoppingToken);
                 
-                _currInterval = _interval;
+                await Task.Delay(interval, stoppingToken);
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
             }
             catch (Exception ex)
             {
-                _logger.SqlExecutionFailed(ex);
+                logger.SqlExecutionFailed(ex);
                 
-                _currInterval = _retryInterval;
+                await Task.Delay(_retryInterval, stoppingToken);
             }
-            
-            await Task.Delay(_currInterval, stoppingToken);
         }
     }
 }
